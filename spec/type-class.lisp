e66818dcbc886dc15ac13cf2676b95b8f8f9373b
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-06-11 22:55:57 +0900

    Fix: spec/type-class.lisp
    Reason:	API is changed.

--- spec/type-class.lisp
+++ spec/type-class.lisp
@@ -36,9 +36,9 @@
     ((==(a a) boolean)
      (/==(a a) boolean))
     (:default == (x y)
-      (not (/== x y)))
+      `(not (/== ,x ,y)))
     (:default /== (x y)
-      (not (== x y))))
+      `(not (== ,x ,y))))
 => EQ
 ,:before (mapc #'fmakunbound '(== /==))
 
@@ -46,13 +46,13 @@
      red yellow green)
 => TRAFFIC-LIGHT
 
-#?(definstance == ((a traffic-light)(b traffic-light))
-    (trivia:match*(a b)
-      ((red red)T)
-      ((green green)T)
-      ((yellow yellow)T)
-      ((_ _) nil)))
-=> ==
+#?(definstance(eq traffic-light)
+    ((==(a b)
+       (trivia:match*(a b)
+	 ((red red)T)
+	 ((green green)T)
+	 ((yellow yellow)T)))))
+=> EQ
 
 #?(== red red) => T
 #?(== green green) => T
@@ -65,12 +65,12 @@
 #?(== red 'not-traffic-light) :signals error
 ,:lazy t
 
-#?(definstance == ((a (maybe *))(b (maybe *)))
-    (trivia:match*(a b)
-      ((nothing nothing)T)
-      (((just x)(just y))(== x y))
-      ((_ _)nil)))
-=> ==
+#?(definstance(eq (maybe *))
+    ((==(a b)
+       (trivia:match*(a b)
+	 ((nothing nothing)T)
+	 (((just x)(just y))`(== ,x ,y))))))
+=> EQ
 
 #?(== nothing nothing) => T
 #?(== (just red)(just red)) => T
@@ -82,26 +82,29 @@
 => YES-NO
 ,:before (mapc #'fmakunbound '(yes-no))
 
-#?(definstance yes-no ((a fixnum))
-    (not(zerop a)))
+#?(definstance(yes-no fixnum)
+    ((yes-no(a)
+       `(not(zerop ,a)))))
 => YES-NO
 
-#?(definstance yes-no ((a list))
-    (not(null a)))
+#?(definstance(yes-no symbol)
+    ((yes-no(a)
+       `(not(null ,a)))))
 => YES-NO
 
-#?(definstance yes-no ((a symbol))
-    (not(null a)))
+#?(definstance(yes-no list)
+    ((yes-no(a)
+       `(not(null ,a)))))
 => YES-NO
 
-#?(definstance yes-no ((a (maybe *)))
-    (unless(eq nothing a)
-      T))
+#?(definstance(yes-no (maybe *))
+    ((yes-no(a)
+       `(not(eq nothing ,a)))))
 => YES-NO
 
-#?(definstance yes-no ((a traffic-light))
-    (unless(eq red a)
-      T))
+#?(definstance(yes-no traffic-light)
+    ((yes-no(a)
+       `(not(eq red ,a)))))
 => YES-NO
 
 #?(yes-no 0) => NIL
@@ -121,14 +124,16 @@
 #?(define-type-class(ord a)()
     ((ord(a)integer))
     (:default ord(x)
-      (data-order x)))
+      `(data-order ,x)))
 => ORD
 ,:before (mapc #'fmakunbound '(ord))
-#?(definstance ord ((a number))
-     a)
+#?(definstance(ord number)
+     ((ord(a)
+	a)))
 => ORD
-#?(definstance ord ((a character))
-    (char-code a))
+#?(definstance(ord character)
+    ((ord(a)
+       `(char-code ,a))))
 => ORD
 
 #?(define-type-class(compare a)(ord)
@@ -138,23 +143,26 @@
      (lte(a a)boolean)
      (gte(a a)boolean))
     (:default compare (x y)
-      (compare (ord x)(ord y)))
+      `(compare (ord ,x)(ord ,y)))
     (:default lt (x y)
-      (< (ord x)(ord y)))
+      `(< (ord ,x)(ord ,y)))
     (:default gt (x y)
-      (> (ord x)(ord y)))
+      `(> (ord ,x)(ord ,y)))
     (:default lte (x y)
-      (<= (ord x)(ord y)))
+      `(<= (ord ,x)(ord ,y)))
     (:default gte (x y)
-      (>= (ord x)(ord y))))
+      `(>= (ord ,x)(ord ,y))))
 => COMPARE
 ,:before (mapc #'fmakunbound '(compare lt gt lte gte))
 
-#?(definstance compare((a number)(b number))
-    (cond
-      ((= a b):eq)
-      ((< a b):lt)
-      (t :gt)))
+#?(definstance(compare number)
+    ((compare(a b)
+       `(let((a ,a)
+	     (b ,b))
+	  (cond
+	    ((= a b):eq)
+	    ((< a b):lt)
+	    (t :gt))))))
 => COMPARE
 
 #?(compare true false) => :GT
@@ -169,9 +177,9 @@
     ((min-bound(a)T)
      (max-bound(a)T))
     (:default min-bound(x)
-      (cadr(introspect-environment:typexpand-1 x)))
+      `(cadr(introspect-environment:typexpand-1 ,x)))
     (:default max-bound(x)
-      (car(last(introspect-environment:typexpand-1 x)))))
+      `(car(last(introspect-environment:typexpand-1 ,x)))))
 => BOUNDED
 ,:before (mapc #'fmakunbound '(min-bound max-bound))
 
@@ -179,11 +187,13 @@
     ((succ(a)a)
      (pred(a)a))
     (:default succ (x)
-      (cadr (nth (1+(data-order x))
-                 (cdr(introspect-environment:typexpand-1(data-type-of x))))))
+      `(let((x ,x))
+	 (cadr (nth (1+(data-order x))
+		    (cdr(introspect-environment:typexpand-1(data-type-of x)))))))
     (:default pred (x)
-      (cadr (nth (1-(data-order x))
-                 (cdr(introspect-environment:typexpand-1(data-type-of x)))))))
+      `(let((x ,x))
+	 (cadr (nth (1-(data-order x))
+		    (cdr(introspect-environment:typexpand-1(data-type-of x))))))))
 => ENUM
 ,:before (mapc #'fmakunbound '(succ pred))
 
@@ -203,21 +213,23 @@
 => FUNCTOR
 ,:before (mapc #'fmakunbound '(fmap))
 
-#?(definstance fmap ((f function)(m (maybe *)))
-    (trivia:ematch m
-      ((just x)(just(funcall f x)))
-      (nothing nothing)))
-=> FMAP
+#?(definstance(functor maybe)
+    ((fmap(f m)
+       (trivia:ematch m
+         ((just x)`(just(funcall ,f ,x)))
+	 (nothing nothing)))))
+=> FUNCTOR
 
 #?(fmap #'1+ nothing) => NOTHING
 #?(fmap #'1+ (just 0)) :be-the (maybe (eql 1))
 #?(fmap #'identity (just 0)) :equivalents (identity (just 0))
 ,:test equal
 
-#?(definstance fmap ((f (function(a)b))(action (io a)))
-    (action result <- action
-            (.return (funcall f result))))
-=> FMAP
+#?(definstance(functor io)
+    ((fmap(f a)
+       `(action result <- ,a
+		(.return (funcall ,f result))))))
+=> FUNCTOR
 
 #?(fmap #'reverse (get-line))
 :satisfies #`(with-input-from-string(*standard-input* "hoge")
@@ -225,22 +237,39 @@
 		  (typep $result 'io-action)
 		  (equal "egoh" (funcall $result))))
 
-#?(definstance fmap ((f function)(g function))
-     (alexandria:compose f g))
-=> FMAP
+#?(definstance(functor function)
+    ((fmap(f g)
+       `(alexandria:compose ,f ,g))))
+=> FUNCTOR
 #?(fmap #'1+ #'1+)
 :satisfies #`(& (functionp $result)
 		(eql 3 (funcall $result 1)))
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 
-#?(definstance fmap ((f function)(l list))
-    (mapcar f l))
-=> FMAP
+#?(definstance(functor list)
+    ((fmap(f l)
+       `(mapcar ,f ,l))))
+=> functor
 #?(fmap #'1+ '(1 2 3)) => (2 3 4)
 ,:test equal
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 #?(fmap #'1+ nil) => NIL
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 #?(fmap #'identity '(1 2 3)) :equivalents (identity '(1 2 3))
 ,:test equal
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 #?(fmap #'identity nil) :equivalents (identity nil)
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 
 #?(defdata counter-maybe(a)
     counter-nothing
@@ -249,41 +278,64 @@
 ,:before (fmakunbound 'counter-just)
 ,:lazy t
 
-#?(definstance fmap ((f function)(cm (counter-maybe *)))
-    (trivia:ematch cm
-      (counter-nothing counter-nothing)
-      ((counter-just counter x)(counter-just (1+ counter)(funcall f x)))))
-=> FMAP
+#?(definstance(functor counter-maybe)
+    ((fmap(f cm)
+       (trivia:ematch cm
+	 (counter-nothing counter-nothing)
+	 ((counter-just counter x)`(counter-just (1+ ,counter)(funcall ,f ,x)))))))
+=> FUNCTOR
 
 #?(fmap #'1+ (counter-just 0 1))
 :satisfies #`(not (equal $result (counter-just 0 1)))
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 
 #?(defvar *a* (fmap (curried-function::section * _ _)'(1 2 3 4)))
 => *A*
-,:lazy nil
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 
 #?(fmap (lambda(f)(funcall f 9)) *a*)
 => (9 18 27 36)
 ,:test equal
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (locally(declare(special *a*))
+	    (call-body)))
+,:lazy t
 
 ;;;; applicative
 #?(define-type-class(applicative f)(functor)
     ((pure(a)(f a))
-     (<*>((function(a)b)(f a))(f b))))
+     (<*>((f(function(a)b))(f a))(f b))))
 => APPLICATIVE
 ,:before (mapc #'fmakunbound '(pure <*>))
 
-#?(definstance <*> ((functor (maybe function))(arg t))
-    (trivia:ematch functor
-      (nothing nothing)
-      ((just f)(fmap f arg))))
-=> <*>
+#?(definstance(applicative maybe)
+    ((<*>(functor arg)
+       (trivia:ematch functor
+         (nothing nothing)
+	 ((just f)`(fmap ,f ,arg))))
+     (pure(x)
+       `(just ,x))))
+=> APPLICATIVE
 
 #?(<*> (just (curried-function::section + 3 _)) (just 9))
 :satisfies #`(equal $result (just 12))
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
 
-#?(<*> (just (curried-function::section + 3 _) nothing))
+#?(<*> (just (curried-function::section + 3 _)) nothing)
 => NOTHING
 
 #?(<*> nothing (just "woot"))
 => NOTHING
+
+#?(<*> (pure (curried-function::section + 3 _)) (just 9))
+:satisfies #`(equal $result (just 12))
+,:around(let((vs-haskell::*subtype-verbose* nil))
+	  (call-body))
+,:lazy t
+
