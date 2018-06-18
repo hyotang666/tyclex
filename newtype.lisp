995b80b88614297dcb6a62010d42e63b58892942
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-06-18 20:16:13 +0900

    Fix: DEFINE-NEWTYPE in newtype.lisp
    Issue:	Unify fails.
    New:	Give up to support some argumetns type constructor.
    TODO:	Support it.

--- newtype.lisp
+++ newtype.lisp
@@ -3,8 +3,6 @@
 (defmacro define-newtype(name lambda-list &body body)
   `(PROGN (DEFTYPE ,name ,lambda-list ,@body)
 	  (DEFMACRO,name(arg)
-	    `(THE (,',name ,',@(mapcar (constantly '*)
-				       (lambda-list:vars<=lambda-list lambda-list)))
-		  ,arg))))
+	    `(THE ,',name ,arg))))
 
 (setf (symbol-function 'denew)#'third)
