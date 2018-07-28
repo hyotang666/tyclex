8571179aff5e18e34117a2ab88bc87e90e94c2ea
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-07-28 19:26:30 +0900

    Refactor: Second commit for TYCLEX.OBJECTS.CELL

--- core/objects/cell/src/tyclex.objects.cell.lisp
+++ core/objects/cell/src/tyclex.objects.cell.lisp
@@ -1,5 +1,23 @@
 (defpackage :tyclex.objects.cell
   (:use :cl)
-  (:export))
+  (:export
+    ;; as type name
+    #:cell
+    ;; as constructor
+    #:make-cell
+    ;; as readers
+    #:signature #:instances #:types #:constraints
+    )
+  )
 (in-package :tyclex.objects.cell)
+
+;;;; CELL
+(defstruct(cell (:predicate nil)
+		(:copier nil)
+		(:conc-name nil))
+  "Represents instance table cell."
+  (signature  (error "SIGNATURE is required.")  :type list :read-only t)
+  (instances  (error "INSTANCES is required.")  :type list :read-only t)
+  (types      (error "TYPES is required.")      :type list :read-only t)
+  (constraints(error "CONSTRAINTS is required."):type list :read-only t))
 
