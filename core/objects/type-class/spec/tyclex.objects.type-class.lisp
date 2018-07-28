2798917d1b49e9c91aaf728dd937b6ec5e5660df
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-07-28 20:08:28 +0900

    Refactor: Skelton for TYCLEX.OBJECTS.TYPE-CLASS

---
+++ core/objects/type-class/spec/tyclex.objects.type-class.lisp
@@ -0,0 +1,5 @@
+(defpackage :tyclex.objects.type-class.spec
+  (:use :cl :jingoh :tyclex.objects.type-class))
+(in-package :tyclex.objects.type-class.spec)
+(setup :tyclex.objects.type-class)
+
---
+++ core/objects/type-class/src/tyclex.objects.type-class.lisp
@@ -0,0 +1,5 @@
+(defpackage :tyclex.objects.type-class
+  (:use :cl)
+  (:export))
+(in-package :tyclex.objects.type-class)
+
