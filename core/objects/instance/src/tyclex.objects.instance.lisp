65451bf90261afd7545e15893294f9d362792e78
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-08-03 13:29:50 +0900

    Refactor: Impls for TYCLEX.OBJECTS.INSTANCE(new).

--- core/objects/instance/src/tyclex.objects.instance.lisp
+++ core/objects/instance/src/tyclex.objects.instance.lisp
@@ -1,5 +1,22 @@
 (defpackage :tyclex.objects.instance
   (:use :cl)
-  (:export))
+  (:export
+    ;; type name
+    #:instance
+    ;; Constructor
+    #:make-type-class-instance
+    ;; Readers
+    #:instance-signature #:instance-definitions #:instance-types #:instance-constraints
+    ))
 (in-package :tyclex.objects.instance)
+
+;;;; INSTANCE
+(defstruct(instance (:predicate nil)
+		    (:constructor make-type-class-instance)
+		    (:copier nil))
+  "Represents interface instance."
+  (signature  	(error "SIGNATURE is required.")  	:type list :read-only t)
+  (definitions	(error "DEFINITIONS is required.")  	:type list :read-only t)
+  (types      	(error "TYPES is required.")      	:type list :read-only t)
+  (constraints	(error "CONSTRAINTS is required.")	:type list :read-only t))
 
