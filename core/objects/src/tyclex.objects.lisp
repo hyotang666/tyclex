13e2a7c4238719a34d5e708848774dd84ce24374
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-08-03 12:01:27 +0900

    Refactor: .objects/src, from .INSTANCE to .INTERFACE

--- core/objects/src/tyclex.objects.lisp
+++ core/objects/src/tyclex.objects.lisp
@@ -2,7 +2,7 @@
 	    `(uiop:define-package :tyclex.objects
                ,@(loop :for key :in options
 		       :collect `(,key #:tyclex.objects.cell
-				       #:tyclex.objects.instance
+				       #:tyclex.objects.interface
 				       #:tyclex.objects.type-class
 				       #:tyclex.objects.io-action
 				       #:tyclex.objects.adt)))))
