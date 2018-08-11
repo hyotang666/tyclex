2b735e58500d33bafc2c442acdeef8e3a5f79de9
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-08-11 22:31:58 +0900

    First commit for TCL.DATA

---
+++ data/src/tcl.data.lisp
@@ -0,0 +1,12 @@
+(defpackage :tcl.data
+  (:use :cl "tyclex.dsl.defdata")
+  (:export
+    #:maybe ; type-name
+    #:just #:nothing ; constructors
+    )
+  )
+(in-package :tcl.data)
+
+(defdata maybe (a)
+  (just a)
+  nothing)
