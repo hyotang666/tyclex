5a74097930aa69f5b1f98836f607cbba19aef88b
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-08-30 14:54:12 +0900

    First commit for TYCLEX.CONDITIONS

--- core/conditions/src/tyclex.conditions.lisp
+++ core/conditions/src/tyclex.conditions.lisp
@@ -1,6 +1,36 @@
 (in-package :cl-user)
 (defpackage :tyclex.conditions
   (:use :cl)
-  (:export))
+  (:export
+    #:tyclex-condition
+    #:tyclex-error
+
+    #:slot-uninitialzied
+    #:missing
+    #:redefinition-warning
+    ))
 (in-package :tyclex.conditions)
+
+(define-condition tyclex-condition(condition)()
+  (:documentation "Root condition for tyclex system."))
+(define-condition tyclex-error(error tyclex-condition)()
+  (:documentation "Root error for tyclex system."))
+
+(define-condition slot-uninitialized(tyclex-error cell-error)()
+  (:documentation "Signaled when slot is not initialized.")
+  (:report (lambda(condition stream)
+	     (format stream "~S uninitialized."
+		     (cell-error-name condition)))))
+
+(define-condition missing(tyclex-error cell-error)()
+  (:documentation "Signaled when object is not found.")
+  (:report (lambda(condition stream)
+	     (format stream "~:(~A~) ~S."
+		     (type-of condition)
+		     (cell-error-name condition)))))
+
+(define-condition redefinition-warning(style-warning cell-error tyclex-condition)()
+  (:documentation "Signaled when object is redefined.")
+  (:report (lambda(condition stream)
+	     (format stream "~S is redefined."(cell-error-name condition)))))
 
