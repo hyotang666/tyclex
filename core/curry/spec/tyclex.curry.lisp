94fd75708c721f941f0bb080492e95f47af8f4ec
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-09-02 00:44:26 +0900

    Add: Some tests for TYCLEX.CURRY

--- core/curry/spec/tyclex.curry.lisp
+++ core/curry/spec/tyclex.curry.lisp
@@ -42,18 +42,8 @@
 
 ; op := Function name, otherwise error
 #?(curry #'+ _ _) :signals error
-; Lambda form is also function name.
-#?(curry (lambda(x y)(+ x y)) _ _)
-:satisfies #`(& ; result is curried function.
-	       (typep $result 'curry)
-	       ; When one argument applied, curried function returned.
-	       (typep (funcall $result 1) 'curry)
-	       ; When all of arguments are applied, body form is evaluated.
-	       (= 3 (funcall (funcall $result 1)
-			     2))
-	       ; We can apply arguments at once.
-	       (= 3 (funcall $result 1 2))
-	       )
+; Lambda form is not valid
+#?(curry (lambda(x y)(+ x y)) _ _) :signals error
 
 ; args := Arguments for `OP`, when there is `_`, result `CURRY`ied function awaits it.
 #?(curry + 1 2 _)
@@ -133,6 +123,189 @@
 
 ;;;; Side-Effects:
 ; Modify lisp environment.
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about DECURRY)
+
+;;;; Description:
+; Make efficient curry form.
+#?(decurry (macroexpand-1 '(curry + _ _)) '(1 2))
+=> (curry + 1 2)
+,:test equal
+#?(decurry (macroexpand-1 '(curry + _ _ _)) '(1 2))
+=> (curry + 1 2 _)
+,:test equal
+
+#+syntax
+(DECURRY form actual-args) ; => result
+
+;;;; Arguments and Values:
+
+; form := Expanded curry form.
+
+; actual-args := argument list.
+
+; result := form.
+
+;;;; Affected By:
+; none
+
+;;;; Side-Effects:
+; none
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about RECURRY)
+
+;;;; Description:
+; Make efficient curry form.
+#?(recurry '(curry + _ _ _) '(1 2))
+=> (curry + 1 2 _)
+,:test equal
+
+#+syntax
+(RECURRY curry-form actual-args) ; => result
+
+;;;; Arguments and Values:
+
+; curry-form := form.
+
+; actual-args := argument list.
+
+; result := form.
+
+;;;; Affected By:
+; none.
+
+;;;; Side-Effects:
+; none
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about CURRY-FORM-P)
+
+;;;; Description:
+; T when form looks like curry form.
+#?(curry-form-p '(curry + _ _)) => T
+#?(curry-form-p 'curry) => NIL
+#?(curry-form-p '(not-curry)) => NIL
+#?(curry-form-p '(curry "invlid")) => T
+
+#+syntax
+(CURRY-FORM-P form) ; => result
+
+;;;; Arguments and Values:
+
+; form := form
+
+; result := BOOLEAN.
+
+;;;; Affected By:
+; none
+
+;;;; Side-Effects:
+; none
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about EXPANDED-CURRY-FORM-P)
+
+;;;; Description:
+; T when form looks like expanded curry form.
+#?(expanded-curry-form-p (macroexpand-1 '(curry + _ _)))
+=> T
+
+#+syntax
+(EXPANDED-CURRY-FORM-P form) ; => result
+
+;;;; Arguments and Values:
+
+; form := 
+
+; result := 
+
+;;;; Affected By:
+
+;;;; Side-Effects:
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about EXPANDED-CURRY-FORM-ARITY)
+
+;;;; Description:
+; Get arity from expanded curry form.
+#?(expanded-curry-form-arity (macroexpand-1 '(curry + _ _)))
+=> 2
+
+#+syntax
+(EXPANDED-CURRY-FORM-ARITY form) ; => result
+
+;;;; Arguments and Values:
+
+; form := 
+
+; result := 
+
+;;;; Affected By:
+
+;;;; Side-Effects:
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about EXPANDED-CURRY-FORM-RETURN-TYPE)
+
+;;;; Description:
+#?(expanded-curry-form-return-type (macroexpand-1 '(curry + _)))
+=> number
+
+#+syntax
+(EXPANDED-CURRY-FORM-RETURN-TYPE form) ; => result
+
+;;;; Arguments and Values:
+
+; form := 
+
+; result := 
+
+;;;; Affected By:
+
+;;;; Side-Effects:
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
+(requirements-about CANONICALIZE-RETURN-TYPE)
+
+;;;; Description:
+#?(canonicalize-return-type '(values number &optional))
+=> NUMBER
+
+#+syntax
+(CANONICALIZE-RETURN-TYPE return-type) ; => result
+
+;;;; Arguments and Values:
+
+; return-type := type-specifier
+
+; result := type-specifier
+
+;;;; Affected By:
+
+;;;; Side-Effects:
 
 ;;;; Notes:
 
