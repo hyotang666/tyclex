cc5b39a2421a59022fc32dc6759c461f7b393f52
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-09-02 22:27:51 +0900

    Fix: TYCLEX
    Old:	Shadowing-import list from TYCLEX.NEWTYPE
    Issue:	List moved to TYCLEX.OBJECTS.NEWTYPE
    New:	Fixed, with shadowing-import-from TYCLEX.OBJECTS.NEWTYPE

--- core/src/tyclex.lisp
+++ core/src/tyclex.lisp
@@ -1,6 +1,6 @@
 (macrolet((def(&rest options)
 	    `(uiop:define-package :tyclex
-               (:shadowing-import-from #:tyclex.newtype #:list)
+               (:shadowing-import-from #:tyclex.objects.newtype #:list)
                ,@(loop :for key :in options
 		       :collect `(,key #:tyclex.curry
 				       #:tyclex.newtype
