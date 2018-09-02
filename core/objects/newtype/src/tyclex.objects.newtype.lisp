138d3643022f5d03554cc40dc3be62ed2e2150e0
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-09-02 15:40:50 +0900

    First commit for TYCLEX.OBJECTS.NEWTYPE

--- core/objects/newtype/src/tyclex.objects.newtype.lisp
+++ core/objects/newtype/src/tyclex.objects.newtype.lisp
@@ -1,6 +1,29 @@
 (in-package :cl-user)
 (defpackage :tyclex.objects.newtype
   (:use :cl)
-  (:export))
+  (:shadow #:list)
+  (:export #:list)
+  (:export #:newtype-type-specifier-p #:add-newtype
+    )
+  )
 (in-package :tyclex.objects.newtype)
 
+(setf(symbol-function 'list)#'cl:list)
+
+(deftype list(&optional a)
+  (declare(ignore a))
+  'cl:list)
+(declaim(ftype(function(&rest t)list)list))
+
+(defvar *newtypes* (make-hash-table :test #'eq))
+
+(defun add-newtype(name)
+  (check-type name (and symbol (not (or keyword boolean))))
+  (setf (gethash name *newtypes*)t))
+
+(defun newtype-type-specifier-p(type-specifier)
+  (labels((ensure-symbol(thing)
+	    (if(atom thing)
+	      thing
+	      (ensure-symbol(car thing)))))
+    (values(gethash (ensure-symbol type-specifier) *newtypes*))))
