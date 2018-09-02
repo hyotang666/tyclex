4358d7e2c330fc3183045c8fbc91e7db12f227cd
Shinichi Sato <hyotang.karakoma@gmail.com>
2018-09-02 21:09:03 +0900

    Add: Tests for TYCLEX.OBJECTS.NEWTYPE

--- core/objects/newtype/spec/tyclex.objects.newtype.lisp
+++ core/objects/newtype/spec/tyclex.objects.newtype.lisp
@@ -3,3 +3,29 @@
 (in-package :tyclex.objects.newtype.spec)
 (setup :tyclex.objects.newtype)
 
+(requirements-about NEWTYPE-TYPE-SPECIFIER-P)
+
+;;;; Description:
+; Return T when argument may newtype type specifier.
+#?(newtype-type-specifier-p "Not newtype type specifier")
+=> NIL
+
+#+syntax
+(NEWTYPE-TYPE-SPECIFIER-P type-specifier) ; => result
+
+;;;; Arguments and Values:
+
+; type-specifier := T
+
+; result := BOOLEAN
+
+;;;; Affected By:
+; Lisp environment.
+
+;;;; Side-Effects:
+; none
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
