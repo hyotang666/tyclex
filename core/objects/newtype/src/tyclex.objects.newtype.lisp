2b3da40d6d91034a32d3526a0954aff12977ba8f
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-10-10 08:18:30 +0900

    Cleanup: Comments.

--- core/objects/newtype/src/tyclex.objects.newtype.lisp
+++ core/objects/newtype/src/tyclex.objects.newtype.lisp
@@ -8,21 +8,27 @@
   )
 (in-package :tyclex.objects.newtype)
 
-(setf(symbol-function 'list)#'cl:list)
-
-(deftype list(&optional a)
-  (declare(ignore a))
-  'cl:list)
+;;;; LIST
+;;; as function.
 (declaim(ftype (function (&rest t)
 			 (values list &optional))
 	       list))
 
+(setf(symbol-function 'list)#'cl:list)
+
+;;; as type.
+(deftype list(&optional a)
+  (declare(ignore a))
+  'cl:list)
+
 (defvar *newtypes* (make-hash-table :test #'eq))
 
+;;;; ADD-NEWTYPE
 (defun add-newtype(name)
   (check-type name (and symbol (not (or keyword boolean))))
   (setf (gethash name *newtypes*)t))
 
+;;;; NEWTYPE-TYPE-SPECIFIER-P
 (defun newtype-type-specifier-p(type-specifier)
   (labels((ensure-symbol(thing)
 	    (if(atom thing)
