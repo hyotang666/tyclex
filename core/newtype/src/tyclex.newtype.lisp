1cc4c48aabdd35e4f6ae1acd45900a1c9c5b85aa
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-03 16:44:05 +0900

    Cleanup: Format.

--- core/newtype/src/tyclex.newtype.lisp
+++ core/newtype/src/tyclex.newtype.lisp
@@ -1,59 +1,64 @@
 (in-package :cl-user)
+
 (defpackage :tyclex.newtype
   (:use :cl)
   (:import-from :tyclex.objects.newtype #:add-newtype)
   (:import-from :tyclex.compute-return-type #:compute-return-type)
   (:import-from :tyclex.unifier
-		#:unify #:find-variable-value #:enwild #:dewild #:make-variable-list)
+                #:unify
+                #:find-variable-value
+                #:enwild
+                #:dewild
+                #:make-variable-list)
   (:export ; main api
-    #:define-newtype
-    #:denew
-    )
-  )
+           #:define-newtype
+           #:denew))
+
 (in-package :tyclex.newtype)
 
-(defmacro define-newtype(name lambda-list &body body)
+(defmacro define-newtype (name lambda-list &body body)
   ;; trivial-syntax-check.
-  (assert(typep name '(and symbol (not (or keyword boolean)))))
-  (assert(listp lambda-list))
+  (assert (typep name '(and symbol (not (or keyword boolean)))))
+  (assert (listp lambda-list))
   ;; body
-  (let((arg(gensym "ARG"))
-       (env(gensym "ENV")))
-    `(EVAL-WHEN(:COMPILE-TOPLEVEL :LOAD-TOPLEVEL :EXECUTE)
-       (ADD-NEWTYPE ',name)
-       (DEFTYPE ,name ,lambda-list ,@body)
+  (let ((arg (gensym "ARG")) (env (gensym "ENV")))
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (add-newtype ',name)
+       (deftype ,name ,lambda-list ,@body)
        ,(<rewinder> name lambda-list)
-       (DEFMACRO,name(,arg &environment ,env)
-	 ,@(if(null lambda-list)
-	     `((declare(ignore ,env))`(THE ,',name ,,arg))
-	     `( `(THE ,(,(intern (format nil "REWIND-~A" name))
-			 (Compute-return-type (expander:expand ,arg ,env)
-					      ,env))
-		      ,,arg)))))))
+       (defmacro ,name (,arg &environment ,env)
+         ,@(if (null lambda-list)
+               `((declare (ignore ,env)) `(the ,',name ,,arg))
+               `(`(the
+                   ,(,(intern (format nil "REWIND-~A" name))
+                     (compute-return-type (expander:expand ,arg ,env) ,env))
+                   ,,arg)))))))
 
-(defun denew(thing)
-  (if(typep thing '(cons (eql the)*))
-    (third thing)
-    thing))
+(defun denew (thing)
+  (if (typep thing '(cons (eql the) *))
+      (third thing)
+      thing))
 
-(defun <rewinder>(name lambda-list)
-  (alexandria:with-gensyms(type-specifier)
-    `(DEFUN ,(intern (format nil "REWIND-~A" name))(,type-specifier)
-       ,@(if(null lambda-list)
-	   `((DECLARE(IGNORE ,type-specifier)) ',name)
-	   (let((required(lambda-fiddle:required-lambda-vars lambda-list)))
-	     (if(null required)
-	       `((DECLARE(IGNORE ,type-specifier))',name)
-	       (let*((variables(Make-variable-list(length required)))
-		     (expanded(millet:type-expand`(,name ,@variables))))
-		 `(`(,',name
-		      ,@(if(equal ',expanded ,type-specifier)
-			  `(,,type-specifier)
-			  (loop :for variable :in ',variables
-				:collect (or (Find-variable-value
-					       variable
-					       (Unify ',(Enwild expanded)
-						      (Enwild ,type-specifier)))
-					     '*)
-				:into args
-				:finally (return(Dewild args)))))))))))))
+(defun <rewinder> (name lambda-list)
+  (alexandria:with-gensyms (type-specifier)
+    `(defun ,(intern (format nil "REWIND-~A" name)) (,type-specifier)
+       ,@(if (null lambda-list)
+             `((declare (ignore ,type-specifier)) ',name)
+             (let ((required (lambda-fiddle:required-lambda-vars lambda-list)))
+               (if (null required)
+                   `((declare (ignore ,type-specifier)) ',name)
+                   (let* ((variables (make-variable-list (length required)))
+                          (expanded (millet:type-expand `(,name ,@variables))))
+                     `(`(,',name
+                         ,@(if (equal ',expanded ,type-specifier)
+                               `(,,type-specifier)
+                               (loop :for variable :in ',variables
+                                     :collect (or (find-variable-value variable
+                                                                       (unify
+                                                                         ',(enwild
+                                                                             expanded)
+                                                                         (enwild
+                                                                           ,type-specifier)))
+                                                  '*)
+                                       :into args
+                                     :finally (return (dewild args)))))))))))))
--- core/newtype/tyclex.newtype.asd
+++ core/newtype/tyclex.newtype.asd
@@ -1,7 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem "tyclex.newtype"
-  :version "0.0.5"
+  :version "0.0.6"
   :depends-on
   (
    "tyclex.objects.newtype"
