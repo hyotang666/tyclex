507e79123fa3fe83a59805452e6fab826f3fb8ea
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-03 16:47:30 +0900

    Cleanup: Format.

--- core/dsl/defio/src/tyclex.dsl.defio.lisp
+++ core/dsl/defio/src/tyclex.dsl.defio.lisp
@@ -1,45 +1,44 @@
 (in-package :cl-user)
+
 (defpackage :tyclex.dsl.defio
   (:use :cl)
   (:import-from :tyclex.newtype #:define-newtype)
-  (:import-from :tyclex.objects.io-action
-		#:io #:io-action #:add-io
-		)
-  (:export
-    #:defio
-    ))
+  (:import-from :tyclex.objects.io-action #:io #:io-action #:add-io)
+  (:export #:defio))
+
 (in-package :tyclex.dsl.defio)
 
-(define-newtype io (a)
-  (declare(ignore a))
+(define-newtype io
+    (a)
+  (declare (ignore a))
   'io-action)
 
 ;;;; DEFIO
-(defmacro defio ((name signature return) &body body)
-  (multiple-value-bind(vars types)(parse-signature signature)
-    `(LOCALLY
-       ;; SBCL emits compiler note about return type `IO-ACTION`.
-       ;; note: type assertion too complex to check:
-       #+sbcl(declare (sb-ext:muffle-conditions sb-ext:compiler-note))
-       (PROCLAIM '(FTYPE(FUNCTION,types(IO ,return)),name))
-       (ADD-IO ',name :TYPE '(IO ,return) :BODY ',body :lambda-list ',vars)
-       (DEFUN,name,vars
-	 (MAKE-INSTANCE 'IO-ACTION
-			:INSTANCE (LAMBDA(),@body)
-			:TYPE '(IO ,return))))))
 
-(defun parse-signature(signature)
-  (labels((rec(signature &optional vars types)
-	    (if(endp signature)
-	      (do-return vars types)
-	      (body (car signature)(cdr signature)vars types)))
-	  (do-return(vars types)
-	    (values (nreverse vars)
-		    (nreverse types)))
-	  (body(sig sig-rest vars types)
-	    (if(find sig lambda-list-keywords :test #'eq)
-	      (rec sig-rest (push sig vars) (push sig types))
-	      (rec sig-rest
-		   (push (car sig)vars)
-		   (push (cadr sig)types)))))
+(defmacro defio ((name signature return) &body body)
+  (multiple-value-bind (vars types)
+      (parse-signature signature)
+    `(locally
+      ;; SBCL emits compiler note about return type `IO-ACTION`.
+      ;; note: type assertion too complex to check:
+      #+sbcl
+      (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+      (proclaim '(ftype (function ,types (io ,return)) ,name))
+      (add-io ',name :type '(io ,return) :body ',body :lambda-list ',vars)
+      (defun ,name ,vars
+        (make-instance 'io-action
+                       :instance (lambda () ,@body)
+                       :type '(io ,return))))))
+
+(defun parse-signature (signature)
+  (labels ((rec (signature &optional vars types)
+             (if (endp signature)
+                 (do-return vars types)
+                 (body (car signature) (cdr signature) vars types)))
+           (do-return (vars types)
+             (values (nreverse vars) (nreverse types)))
+           (body (sig sig-rest vars types)
+             (if (find sig lambda-list-keywords :test #'eq)
+                 (rec sig-rest (push sig vars) (push sig types))
+                 (rec sig-rest (push (car sig) vars) (push (cadr sig) types)))))
     (rec signature)))
--- core/dsl/defio/tyclex.dsl.defio.asd
+++ core/dsl/defio/tyclex.dsl.defio.asd
@@ -1,7 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem "tyclex.dsl.defio"
-  :version "0.0.1"
+  :version "0.0.2"
   :depends-on
   (
    "tyclex.objects.io-action"   ; Module for io-action object.
