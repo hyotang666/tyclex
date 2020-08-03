7b0506db980e956c8746f3ad4716ce378500786e
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-03 16:04:34 +0900

    Cleanup: Format.

--- core/curry/src/tyclex.curry.lisp
+++ core/curry/src/tyclex.curry.lisp
@@ -1,19 +1,26 @@
 (in-package :cl-user)
+
 (defpackage :tyclex.curry
   (:use :cl)
-  (:export
-    ;; Main API
-    #:curry
-    ;; Helpers
-    #:function-type-of #:function-type #:canonicalize-return-type
-    ;; Decurry
-    #:decurry #:recurry #:curry-form-p #:expanded-curry-form-p
-    #:expanded-curry-form-arity #:expanded-curry-form-return-type
-    #:first-promised-curry
-    ))
+  (:export ;; Main API
+           #:curry
+           ;; Helpers
+           #:function-type-of
+           #:function-type
+           #:canonicalize-return-type
+           ;; Decurry
+           #:decurry
+           #:recurry
+           #:curry-form-p
+           #:expanded-curry-form-p
+           #:expanded-curry-form-arity
+           #:expanded-curry-form-return-type
+           #:first-promised-curry))
+
 (in-package :tyclex.curry)
 
 ;;;; CURRY data structure.
+
 (defclass curry ()
   ((function :initarg :function :reader curried-function)
    (arity :initarg :arity :reader arity)
@@ -24,168 +31,160 @@
   (c2mop:set-funcallable-instance-function c (curried-function c)))
 
 ;;;; CURRY
+
 (defmacro curry (&whole whole op &rest args)
   ;; Trivial syntax check.
   (check-type op symbol)
   (assert args)
   ;; body
-  (<Section-Form> op args whole))
+  (<section-form> op args whole))
 
 ;;; <Section-Form>
-(defun <Section-Form>(op args whole)
-  (let*((gensyms(underscore-gensyms args))
-	(optional-lambda-list(optional-lambda-list gensyms)))
+
+(defun <section-form> (op args whole)
+  (let* ((gensyms (underscore-gensyms args))
+         (optional-lambda-list (optional-lambda-list gensyms)))
     (if gensyms
-      (<Curry-Form> (<Section-Body-Form> op args gensyms)
-		    optional-lambda-list
-		    (or (third(function-type-of op))
-			(third(introspect-environment:function-type op)))
-		    whole)
-      `(,op ,@args))))
+        (<curry-form> (<section-body-form> op args gensyms)
+                      optional-lambda-list
+                      (or (third (function-type-of op))
+                          (third (introspect-environment:function-type op)))
+                      whole)
+        `(,op ,@args))))
 
-(defun underscore-gensyms(args)
-  (alexandria:make-gensym-list(count-if #'underscorep args)))
+(defun underscore-gensyms (args)
+  (alexandria:make-gensym-list (count-if #'underscorep args)))
 
-(defun underscorep (thing)
-  (and (symbolp thing)
-       (string= "_" thing)))
+(defun underscorep (thing) (and (symbolp thing) (string= "_" thing)))
 
-(defun optional-lambda-list(lambda-list)
-  (mapcar (lambda(x)
-	    `(,x NIL ,(gensym (format nil "~A-P"x))))
-	  lambda-list))
+(defun optional-lambda-list (lambda-list)
+  (mapcar (lambda (x) `(,x nil ,(gensym (format nil "~A-P" x)))) lambda-list))
 
 ;;; <Section-Body-Form>
-(defun <Section-Body-Form>(op args gensyms)
-  (labels((rec(args gensyms &optional acc)
-	    (if(endp args)
-	      (nreverse acc)
-	      (body(car args)(cdr args)gensyms acc)))
-	  (body(arg rest gensyms acc)
-	    (if(underscorep arg)
-	      (rec rest (cdr gensyms)(push (car gensyms)acc))
-	      (rec rest gensyms (push arg acc)))))
+
+(defun <section-body-form> (op args gensyms)
+  (labels ((rec (args gensyms &optional acc)
+             (if (endp args)
+                 (nreverse acc)
+                 (body (car args) (cdr args) gensyms acc)))
+           (body (arg rest gensyms acc)
+             (if (underscorep arg)
+                 (rec rest (cdr gensyms) (push (car gensyms) acc))
+                 (rec rest gensyms (push arg acc)))))
     `((,op ,@(rec args gensyms)))))
 
 ;;; <Curry-Form>
-(defun <Curry-Form> (body optional-lambda-list return-type whole)
-  (let((curry (gensym "CURRY")))
-    (labels((ENTRY-POINT(list)
-	      (if(endp list)
-		(<BODY-FORM> body)
-		`(LABELS((,curry(&OPTIONAL ,@list)
-			   (IF ,(caddar list)
-			     ,(rec (cdr list))
-			     (MAKE-INSTANCE 'CURRY
-					    :FUNCTION #',curry
-					    :ARITY ,(length list)
-					    :RETURN-TYPE ',return-type
-					    ))))
-		   ',whole ; for decurry.
-		   (MAKE-INSTANCE 'CURRY
-				  :FUNCTION #',curry
-				  :ARITY ,(length list)
-				  :RETURN-TYPE ',return-type
-				  ))))
-	    (REC(list)
-	      (if(endp list)
-		(<body-form> body)
-		`(IF,(caddar list)
-		   ,(REC (cdr list))
-		   ,(ENTRY-POINT list))))
-	    (<BODY-FORM>(body)
-		(if(cdr body)
-		  `(LOCALLY ,@body)
-		  (car body)))
-	    )
-      (ENTRY-POINT optional-lambda-list))))
+
+(defun <curry-form> (body optional-lambda-list return-type whole)
+  (let ((curry (gensym "CURRY")))
+    (labels ((entry-point (list)
+               (if (endp list)
+                   (<body-form> body)
+                   `(labels ((,curry (&optional ,@list)
+                               (if ,(caddar list)
+                                   ,(rec (cdr list))
+                                   (make-instance 'curry
+                                                  :function #',curry
+                                                  :arity ,(length list)
+                                                  :return-type ',return-type))))
+                      ',whole ; for decurry.
+                      (make-instance 'curry
+                                     :function #',curry
+                                     :arity ,(length list)
+                                     :return-type ',return-type))))
+             (rec (list)
+               (if (endp list)
+                   (<body-form> body)
+                   `(if ,(caddar list)
+                        ,(rec (cdr list))
+                        ,(entry-point list))))
+             (<body-form> (body)
+               (if (cdr body)
+                   `(locally ,@body)
+                   (car body))))
+      (entry-point optional-lambda-list))))
 
 ;;;; DECURRY
+
 (defun decurry (form actual-args)
-  (labels((rec(if count)
-	    (if(zerop count)
-	      (if(eq 'if (car if))
-		(fourth if)
-		if)
-	      (rec(third if)(1- count)))))
-    (expander:walk-sublis (loop :for var :in (cdr (second(first(second form))))
-				:for arg :in actual-args
-				:collect `(,(car var),arg))
-			  (rec (third(first(second form)))
-			       (length actual-args)))))
+  (labels ((rec (if count)
+             (if (zerop count)
+                 (if (eq 'if (car if))
+                     (fourth if)
+                     if)
+                 (rec (third if) (1- count)))))
+    (expander:walk-sublis
+      (loop :for var :in (cdr (second (first (second form))))
+            :for arg :in actual-args
+            :collect `(,(car var) ,arg))
+      (rec (third (first (second form))) (length actual-args)))))
 
-(defun recurry(curry-form actual-args)
-  (if(null actual-args)
-    curry-form
-    (let*((underscore-num(count-if #'underscorep (cddr curry-form)))
-	  (arg-num(length actual-args))
-	  (diff(- underscore-num arg-num)))
-      (flet((UNDERSCORE-TO-ACTUAL-ARG(whole gensyms)
-	      (loop :for elt :in whole
-		    :when (and (symbolp elt)
-			       (string= "_" elt))
-		    :collect (or (pop gensyms)
-				 elt)
-		    :else :collect elt)))
-	(if (minusp diff)
-	  (error "Too much args. ~S ~S"curry-form actual-args)
-	   (UNDERSCORE-TO-ACTUAL-ARG curry-form actual-args))))))
+(defun recurry (curry-form actual-args)
+  (if (null actual-args)
+      curry-form
+      (let* ((underscore-num (count-if #'underscorep (cddr curry-form)))
+             (arg-num (length actual-args))
+             (diff (- underscore-num arg-num)))
+        (flet ((underscore-to-actual-arg (whole gensyms)
+                 (loop :for elt :in whole
+                       :when (and (symbolp elt) (string= "_" elt))
+                         :collect (or (pop gensyms) elt)
+                       :else
+                         :collect elt)))
+          (if (minusp diff)
+              (error "Too much args. ~S ~S" curry-form actual-args)
+              (underscore-to-actual-arg curry-form actual-args))))))
 
-(defun curry-form-p(form)
-  (typep form '(cons (eql curry) *)))
+(defun curry-form-p (form) (typep form '(cons (eql curry) *)))
 
-(defun expanded-curry-form-p(form)
+(defun expanded-curry-form-p (form)
   (and (listp form)
        (eq 'labels (car form))
-       (let((body (cddr form)))
-	 (and (every #'listp body)
-	      (= 2 (length body))
-	      (eq 'make-instance (caadr body))
-	      (equal (cadadr body) '(quote curry))))))
+       (let ((body (cddr form)))
+         (and (every #'listp body)
+              (= 2 (length body))
+              (eq 'make-instance (caadr body))
+              (equal (cadadr body) ''curry)))))
 
-(defun expanded-curry-form-arity(form)
-  (let*((make-instance(fourth form))
-	(arity(getf make-instance :arity)))
+(defun expanded-curry-form-arity (form)
+  (let* ((make-instance (fourth form)) (arity (getf make-instance :arity)))
     (introspect-environment:constant-form-value arity)))
 
-(defun expanded-curry-form-return-type(form)
-  (let*((make-instance(fourth form))
-	(return-type(getf make-instance :return-type)))
-    (canonicalize-return-type(introspect-environment:constant-form-value return-type))))
+(defun expanded-curry-form-return-type (form)
+  (let* ((make-instance (fourth form))
+         (return-type (getf make-instance :return-type)))
+    (canonicalize-return-type
+      (introspect-environment:constant-form-value return-type))))
 
-(defun first-promised-curry(expanded)
-  (destructuring-bind(op ((name (key first . rest)body)) origin main)expanded
-    `(,op ((,name(,(car first),key ,@rest)
-	     ,(third body)))
-	  ,origin
-	  ,(getf main :function))))
+(defun first-promised-curry (expanded)
+  (destructuring-bind
+      (op ((name (key first . rest) body)) origin main)
+      expanded
+    `(,op ((,name (,(car first) ,key ,@rest) ,(third body))) ,origin
+      ,(getf main :function))))
 
 ;;;; CANONICALIZE-RETURN-TYPE
-(defun canonicalize-return-type(return-type)
-  (flet((ENSURE-T(thing)
-	  (sublis '((* . T)
-		    (simple-vector . vector)
-		    (simple-array . array)
-		    (simple-string . string)
-		    (base-string . string)
-		    )
-		  thing)))
-    (if(typep return-type '(CONS (EQL VALUES)T))
-      (if(typep return-type '(CONS * (CONS (EQL &OPTIONAL) T)))
-	(ENSURE-T (caddr return-type))
-	(ENSURE-T (cadr return-type)))
-      (ENSURE-T return-type))))
+
+(defun canonicalize-return-type (return-type)
+  (flet ((ensure-t (thing)
+           (sublis
+             '((* . t) (simple-vector . vector) (simple-array . array)
+               (simple-string . string) (base-string . string))
+             thing)))
+    (if (typep return-type '(cons (eql values) t))
+        (if (typep return-type '(cons * (cons (eql &optional) t)))
+            (ensure-t (caddr return-type))
+            (ensure-t (cadr return-type)))
+        (ensure-t return-type))))
 
 ;;;; FUNCTION-TYPE
+
 (defmacro function-type (name args return)
   ;; trivial syntax checks.
   (check-type name (and symbol (not (or keyword boolean))))
-  (check-type args (or (eql *)list))
+  (check-type args (or (eql *) list))
   (check-type return (or symbol list))
   ;; body
-  `(PROGN (SETF (GET ',name 'FTYPE)'(FUNCTION ,args ,return))
-	  ',name))
+  `(progn (setf (get ',name 'ftype) '(function ,args ,return)) ',name))
 
-(defun function-type-of(name)
-  (get name 'ftype))
-
+(defun function-type-of (name) (get name 'ftype))
--- core/curry/tyclex.curry.asd
+++ core/curry/tyclex.curry.asd
@@ -1,7 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem "tyclex.curry"
-  :version "0.0.1"
+  :version "0.0.2"
   :depends-on
   (
    "closer-mop"                 ; Wrapper for meta object protocols.
