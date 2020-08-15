2ac52707ce97e0e39088de89059166cb1d7501f5
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-15 14:30:05 +0900

    Cleanup: Format monad-plus.

--- modules/monad-plus/src/tcl.monad-plus.lisp
+++ modules/monad-plus/src/tcl.monad-plus.lisp
@@ -1,41 +1,46 @@
 (tcl:defpackage :tcl.monad-plus
   (:use :tcl #:tcl.monad)
   (:shadowing-import-from :tcl.monad #:return #:do)
-  (:export
-    ;; type-class name
-    #:monad+
-    ;; interfaces
-    #:mzero #:mplus
-    ;; predicate
-    #:monad+-p
-    ;; helpers
-    #:guard #:lc
-    ))
+  (:export ;; type-class name
+           #:monad+
+           ;; interfaces
+           #:mzero
+           #:mplus
+           ;; predicate
+           #:monad+-p
+           ;; helpers
+           #:guard
+           #:lc))
+
 (in-package :tcl.monad-plus)
 
-(define-type-class(monad+ m)((monad m))
-  ((mzero()(m a))
-   (mplus((m a)(m a))(m a))))
+(define-type-class (monad+ m) ((monad m))
+  ((mzero () (m a))
+   (mplus ((m a) (m a)) (m a))))
 
 ;;;; Helpers
-(defmacro guard(bool)
+
+(defmacro guard (bool)
   `(if ,bool
-     (return nil)
-     (mzero)))
+       (return nil)
+       (mzero)))
 
 (defmacro lc (&rest expression*)
-  `(do ,@(labels((rec(list &optional acc)
-		   (if(endp list)
-		     (nreverse acc)
-		     (if(and (symbolp(second list))
-			     (string= '#:<- (second list)))
-		       (rec (cdddr list)(list* (third list)(second list)(first list)acc))
-		       (rec (cdr list)(cons `(guard ,(car list))acc))))))
-	   (rec (cddr expression*)))
-     (return ,(car expression*))))
+  `(do
+    ,@(labels ((rec (list &optional acc)
+                 (if (endp list)
+                     (nreverse acc)
+                     (if (and (symbolp (second list))
+                              (string= '#:<- (second list)))
+                         (rec (cdddr list)
+                              (list* (third list) (second list) (first list)
+                                     acc))
+                         (rec (cdr list) (cons `(guard ,(car list)) acc))))))
+        (rec (cddr expression*)))
+    (return ,(car expression*))))
 
 ;;;; Instances
-(definstance(monad+ list)
-  ((mzero()nil)
-   (mplus(a b)`(append ,a ,b))))
 
+(definstance (monad+ list)
+  ((mzero () nil)
+   (mplus (a b) `(append ,a ,b))))
