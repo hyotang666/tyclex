9e71085fbbe75bbdfab89af5084b025a0fa394e5
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-15 14:34:28 +0900

    Cleanup: Format bounded.

--- modules/bounded/src/tcl.bounded.lisp
+++ modules/bounded/src/tcl.bounded.lisp
@@ -1,12 +1,11 @@
 (tcl:defpackage :tcl.bounded
   (:use :tcl)
   (:export))
+
 (in-package :tcl.bounded)
 
-(define-type-class(bounded a)()
-  ((min-bound(a)T)
-   (max-bound(a)T))
-  (:default min-bound(x)
-	    `(car (adt-constructors (find-adt ,x))))
-  (:default max-bound(x)
-	    `(car (last(adt-constructors (find-adt ,x))))))
+(define-type-class (bounded a) ()
+  ((min-bound (a) t)
+   (max-bound (a) t))
+  (:default min-bound (x) `(car (adt-constructors (find-adt ,x))))
+  (:default max-bound (x) `(car (last (adt-constructors (find-adt ,x))))))
