6a791796fc2754917bc25aa99a17b91aafc79355
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-15 14:54:54 +0900

    Cleanup: Format zip-list.

--- modules/zip-list/src/tcl.zip-list.lisp
+++ modules/zip-list/src/tcl.zip-list.lisp
@@ -1,24 +1,21 @@
 (tcl:defpackage :tcl.zip-list
   (:use #:tcl #:tcl.applicative #:tcl.functor)
-  (:export
-    ;; type-name
-    #:zip-list
-    ))
+  (:export ;; type-name
+           #:zip-list))
+
 (in-package :tcl.zip-list)
 
-(define-newtype zip-list(&optional a)
-  (declare(ignore a))
-  'list)
+(define-newtype zip-list (&optional a) (declare (ignore a)) 'list)
 
-(definstance(functor zip-list)
-  ((fmap(f zl)
-     `(zip-list (fmap ,f ,(denew zl))))))
+(definstance (functor zip-list)
+  ((fmap (f zl) `(zip-list (fmap ,f ,(denew zl))))))
 
-(definstance(applicative zip-list)
-  ((pure(x)
-     `(series:series ,x))
-   (<*>(fs xs)
-     (alexandria:with-gensyms(fn)
-       `(zip-list (let((,fn ,fs))
-		    (series:collect (series:map-fn t #'funcall (series:scan ,fn)
-						   (series:scan ,xs)))))))))
+(definstance (applicative zip-list)
+  ((pure (x) `(series:series ,x))
+   (<*> (fs xs)
+     (alexandria:with-gensyms (fn)
+       `(zip-list
+         (let ((,fn ,fs))
+           (series:collect
+            (series:map-fn t #'funcall (series:scan ,fn)
+                           (series:scan ,xs)))))))))
--- modules/zip-list/tcl.zip-list.asd
+++ modules/zip-list/tcl.zip-list.asd
@@ -1,6 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem "tcl.zip-list"
+  :version "0.0.1"
   :depends-on
   (
    "tcl"
