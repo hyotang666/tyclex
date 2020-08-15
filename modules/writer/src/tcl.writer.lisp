622a6f5cd40af81c5834b357872bc449914044b0
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-15 14:56:32 +0900

    Cleanup: Format writer.

--- modules/writer/src/tcl.writer.lisp
+++ modules/writer/src/tcl.writer.lisp
@@ -1,21 +1,21 @@
 (tcl:defpackage :tcl.writer
   (:use #:tcl #:tcl.monad #:tcl.monoid)
   (:shadowing-import-from :tcl.monad #:do #:return)
-  (:export
-    ;; newtype
-    #:writer
-    ))
+  (:export ;; newtype
+           #:writer))
+
 (in-package :tcl.writer)
 
-(define-newtype writer(&optional w a)
-  `(cons ,a ,w))
+(define-newtype writer (&optional w a) `(cons ,a ,w))
 
-(definstance(monad (writer w) :constraints (monoid w))
-  ((return(x)
-     `(writer (cons ,x (mempty))))
-   (>>=(writer f)
-     (alexandria:with-gensyms(x v y v%)
-       `(destructuring-bind(,x . ,v),writer
-	  (destructuring-bind(,y . ,v%)(funcall ,f ,x)
-	    (writer(cons ,y (mappend ,v ,v%)))))))))
-
+(definstance (monad (writer w) :constraints (monoid w))
+  ((return (x) `(writer (cons ,x (mempty))))
+   (>>= (writer f)
+     (alexandria:with-gensyms (x v y v%)
+       `(destructuring-bind
+            (,x . ,v)
+            ,writer
+          (destructuring-bind
+              (,y . ,v%)
+              (funcall ,f ,x)
+            (writer (cons ,y (mappend ,v ,v%)))))))))
--- modules/writer/tcl.writer.asd
+++ modules/writer/tcl.writer.asd
@@ -1,6 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem "tcl.writer"
+  :version "0.0.1"
   :depends-on
   (
    "tcl"
