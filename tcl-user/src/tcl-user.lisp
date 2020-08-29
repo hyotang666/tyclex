0faa879e6648e0c1d35ee3b792e63183fc629f88
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-08-29 14:07:26 +0900

    Change: Package tcl-user to export symbols.

--- tcl-user/src/tcl-user.lisp
+++ tcl-user/src/tcl-user.lisp
@@ -1,7 +1,13 @@
-(tcl:defpackage :tcl-user
-  (:use :tcl #:tcl.applicative #:tcl.compare #:tcl.eq #:tcl.io #:tcl.monad-plus #:tcl.ord
-	#:tcl.bounded #:tcl.enum #:tcl.functor #:tcl.monad #:tcl.monoid #:tcl.zip-list)
-  (:shadowing-import-from :tcl.monad #:return #:do)
-  (:export))
-(in-package :tcl-user)
+(in-package :cl-user)
 
+(macrolet ((def (&rest options)
+             `(uiop:define-package :tcl-user
+                (:shadowing-import-from :tyclex.objects.newtype #:list)
+                (:shadowing-import-from :tcl.monad #:return #:do)
+                ,@(loop :for key :in options
+                        :collect `(,key #:tcl #:tcl.applicative #:tcl.compare
+                                   #:tcl.eq #:tcl.io #:tcl.monad-plus #:tcl.ord
+                                   #:tcl.bounded #:tcl.enum #:tcl.functor
+                                   #:tcl.monad #:tcl.monoid #:tcl.zip-list
+                                   #:tcl.writer #:tcl.diff-list #:tcl.state)))))
+  (def :use :reexport))
